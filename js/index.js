4078217fee111428c9fc55f6402081496bea4ded
Si Thu Tin <sithu5897@gmail.com>
2022-06-16 19:22:33 +0630

    img full view

--- js/index.js
+++ js/index.js
@@ -18,14 +18,26 @@ window.onclick = function(event) {
   }
 // ------------------ get thumbnail and main image elements --------------------
 const mainImg = document.getElementById("mainImg");
+const fullView = document.getElementById("fullView");
 const thumbnailDiv = document.getElementsByClassName("product__thumbnail");
 const thumbnailChildNodes = thumbnailDiv[0].childNodes;
+const fullImg = document.getElementById("fullImg");
+const imgAry = [1,2,3,4];
+let imgIndex = 0;
 const thumbnailImg = [];
 thumbnailChildNodes.forEach(element => {
   if(element.localName == 'img') {
     thumbnailImg.push(element);
   }
 });
+const fullThumbnailDiv = document.getElementsByClassName("full-img-view__thumbnail");
+const fullThumbnailChildNodes = fullThumbnailDiv[0].childNodes;
+const fullThumbnail =[];
+fullThumbnailChildNodes.forEach(element => {
+  if(element.localName =='img') {
+    fullThumbnail.push(element);
+  }
+})
 
 // --------------------------- change img function -----------------------------
 function changeImg(name) {
@@ -35,11 +47,32 @@ function changeImg(name) {
     }
   }
     const idName = `img${name}`
-    const thumbnail = document.getElementById(idName);
-    thumbnail.classList.add("product__thumbnail--active")
+    let thumbnail = document.getElementById(idName);
     mainImg.src = `./assets/images/image-product-${name}.jpg`
+    thumbnail.classList.add("product__thumbnail--active");
 }
 
 function showFullImg() {
-  console.log(mainImg)
+  for(let i = 0; i< 4; i++) {
+    if(fullThumbnail[i].classList.contains("product__thumbnail--active")) {
+      fullThumbnail[i].classList.remove("full-img-view__thumbnail--active")
+    }
+  }
+  console.log(fullThumbnail)
+  fullImg.src = mainImg.src
+  fullView.classList.add("full-img-view--active")
+}
+
+function closeFullImg() {
+  fullView.classList.remove("full-img-view--active");
+}
+
+function changeFullViewImg(name) {
+  if( i < imgAry.length) {
+    fullImg.src = `./assets/images/image-product-${imgAry[imgIndex]}.jpg`;
+    imgIndex++;
+  } else {
+    imgIndex = 0;
+    fullImg.src = `./assets/images/image-product-${imgAry[imgIndex]}.jpg`;
+  }
 }
